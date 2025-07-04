7ce474ef4f3374966741d0085bb7ee5ab4fc0055
Blooming0 <hassanalhosyni@gmail.com>
2025-07-04 17:53:58 +0300

    End of the Project with the Extra Credit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -25,13 +26,80 @@ public class Main {
         System.out.println("▶ Game Mode      : Player vs Computer");
         System.out.println("▶ Board Size     : 3 x 3");
         System.out.println("▶ Player Symbols : X (Player 1), O (Computer)");
-        System.out.println("▶ Instructions   : Enter one of the positions that shows in the Board below");
+        System.out.println("▶ Instructions   : Enter one of the positions that will shows in the Board Game");
         System.out.println("▶ Goal           : First to align 3 symbols wins!");
+        System.out.println("▶ Round          : you can select 1 or 3 Rounds");
         System.out.println("=====================================");
         System.out.println("        Let's Begin the Game!        ");
         System.out.println();
-        String result = board();
-        System.out.println(result);
+
+        int round = 0;
+        int counter = 0;
+        while (!(round == 1 || round == 3)) {
+            System.out.println("How many round you wana play?");
+            try {
+                round = in.nextInt();
+                if (round != 1 && round != 3) {
+                    System.out.println("⚠ Please enter either 1 or 3 only.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("⚠ Invalid input! Please enter a number (1 or 3).");
+            } catch (Exception e) {
+                System.out.println("⚠ Something went wrong. Please try again.");
+            } finally {
+                in.nextLine(); // to Clear the invalid input (important!)
+            }
+        }
+
+        if(round == 3){//
+            String[] gameRecord = new String[round];
+            int x_Record = 0;
+            int o_Record = 0;
+            int tie_Record = 0;
+            while (counter < round){
+                System.out.println("============{Round "+(counter+1)+"}===============");
+                String result = board();
+                gameRecord[counter] = result;
+               if (result.contains("X")) {
+                    gameRecord[counter] = "X";
+                    System.out.println("🎉 Congratulations! You (X) won this Round{"+(counter+1)+"} ! Keep it up🔥");
+               }else if (result.contains("O")) {
+                    gameRecord[counter] = "O";
+                   System.out.println("💻 The computer (O) wins the this Round{"+(counter+1)+"}! try your Best🤧");
+               }else {
+                    gameRecord[counter] = "Tie";
+                   System.out.println("At this Round{"+(counter+1)+"}🤝 It's a tie between you and the computer! ");
+
+               }
+                counter++;
+
+            }
+            for (String s : gameRecord) {
+                if ("X".equals(s)) {
+                    x_Record++;
+                } else if ("O".equals(s)) {
+                    o_Record++;
+                } else if("Tie".equals(s)) {
+                    tie_Record++;
+                }
+            }
+
+            if(x_Record > o_Record){
+                showRecordAndWinner("X",x_Record,gameRecord);//send the winner & record of the game to print it .
+            } else if (x_Record < o_Record) {
+                showRecordAndWinner("O",o_Record,gameRecord);
+            }else{
+                showRecordAndWinner("Tie",tie_Record,gameRecord);
+            }
+        }else{
+            String result = board();
+            System.out.println(result);
+        }
+
+
+
+
+
     }
     public static String board(){
         String [][]board = {
@@ -39,6 +107,7 @@ public class Main {
                 {"4", "5", "6"},
                 {"7", "8", "9"},
         };
+
         winner = getWinner(board);
         return winner.equals("Tie") ? ("Game ended in Tie") : "\n{"+winner +"} wins the game!";
     }
@@ -63,24 +132,30 @@ public class Main {
     public static String getWinner(String [][]board){
         char turn = 'X';
         while(gameStatus(board)){
-            //show the board :
+
             showBoard(board);
-            String position ;
-            //message only if it's user turn
-            if(turn == 'X'){
-                System.out.println("Enter a number from the board");//I will check if the number is valid or no .
+            String position = "" ;
+
+            if (turn == 'X') {
+                System.out.println("Enter a number from the board:");
                 position = in.nextLine();
-            }else{
+                if (!position.matches("[1-9]")) {
+                    System.out.println("⚠ Invalid input! Please enter a number from 1 to 9.");
+                    continue;
+                }
+            }else {
                 position = generateComputerRandomNumberFrom1To9();
             }
             if(validatPosition(turn ,position, board)){
-                //swapping the turn using ternary operator.
-                if(turn != 'X'){
-                    System.out.println("Computer(O) Selection board");
+
+                if(turn != 'X'){//char is not a reference variable like string so you can compare like this ,the char is considered as primitive data type
+                    System.out.println("Computer (O) selected position " + position);
+                }else{
+                    System.out.println("Player (X) selected position " + position);
                 }
                 turn = (turn == 'X')?('O'):('X');
             }
-        }//while loop will break if and only if the game (END or Tie).
+        }
         showBoard(board);
         return winner;
     }
@@ -88,7 +163,6 @@ public class Main {
         for (int i = 0; i < board.length;i++) {
             for (int j = 0; j < board.length;j++) {
                     if(board[i][j].equals(position)){
-//                  updating method, you only need to check if the number consider as a valid number or not
                         board[i][j] = String.valueOf(turn);// then update statement .
                         return true;//this will end the method
                     }
@@ -137,5 +211,25 @@ public class Main {
         }
         winner = "Tie";
         return false;
+    }
+    public static void showRecordAndWinner(String winner,int numberOfRoundWin , String[] gameRecord){
+        System.out.println("\n\n============{ Record of the Rounds }============\n");
+        //print the Record of the game :
+        for (int i = 0; i < gameRecord.length; i++) {
+            System.out.print("at the round {"+(i+1)+"} the Round goes to { "+gameRecord[i]+" }\n");
+        }
+
+        System.out.println("============{ OVERALL RESULT }============");
+        if (winner.equals("X")) {
+            System.out.println("🎉 Congratulations! You (X) won the game!");
+            System.out.println("👏 Well played! You won " + numberOfRoundWin + " out of 3 rounds against the computer.");
+        } else if (winner.equals("O")) {
+            System.out.println("💻 The computer (O) wins the game!");
+            System.out.println("Don't worry, better luck next time! The computer won " + numberOfRoundWin + " out of 3 rounds.");
+        } else {
+            System.out.println("🤝 It's a tie between you and the computer!");
+            System.out.println("Each of you won " + numberOfRoundWin + " round(s). Great effort!");
+        }
+
     }
 }
